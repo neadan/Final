000205cd6b9b3ba10012938365a8f2316b923f4e
ManuelG <manuel5.guzman1@gmail.com>
2021-10-30 14:18:54 -0400

    Final submission

--- app/books/routes.py
+++ app/books/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, Response, jsonify
+
+from app.models import Fiction, NonFiction, db
 
 book_bp = Blueprint('books', __name__, url_prefix='/api/v1/books')
 
@@ -10,12 +12,22 @@ def books_fiction():
     ###### Return all books that are Fiction ######
     ###### Accept the "author" query parameter, which returns all books that are written by that specific author ######
     if request.method == 'GET':
-        return "implement this GET (fiction)"
+        query_params = request.args
+        if not query_params:
+            books = Fiction.query.all()
+            result = [book.to_dict() for book in books]
+            return jsonify(result)
 
     ###### Create a new book in the database ######
     ###### Don't forget to return the full path of the resource in the location header ######
     if request.method == 'POST':
-        return "implement this POST (fiction)"
+        data = request.form
+        book_to_add = Fiction(title=data['title'], author=data['author'], year=data['year'])
+        db.session.add(book_to_add)
+        db.session.commit()
+        response = Response(status=200)
+        response.headers['location'] = f"books/{book_to_add.id}"
+        return response
 
 
 @book_bp.route("/fiction/<int:book_id>", methods=['GET', 'DELETE'])
@@ -23,15 +35,23 @@ def book_id_fiction(book_id):
     ###### Return the book with book_id ######
     ###### If no such book exists, return the appropriate response as we have seen in class ######
     if request.method == 'GET':
-        return "implement this GET (book_id)"
+        book = Fiction.query.get(book_id)
+        return jsonify(book.to_dict())
+
     ###### Delete the book with no ######
     ###### If no such book exists, return the appropriate response as we have seen in class ######
     if request.method == 'DELETE':
-        return "implement this DELETE (book_id)"
+        book = Fiction.query.get(book_id)
+        if book is None:
+            return Response(status=404)
+        Fiction.delete(book)
+        db.session.remove(book)
+        db.session.commit()
+        return Response(status=200)
 
 
 @book_bp.route("/all", methods=['GET'])
 def books():
     ###### Return all books (whether they are fiction or non-fiction ######
     if request.method == 'GET':
-        return "implement this GET (all)"
+        return "implement this GET(all)"
--- app/models.py
+++ app/models.py
@@ -11,18 +11,44 @@ db = SQLAlchemy()
 # Have both models inherit from db.Model as well as the Book class below. This Book class will be used to serialize
 # our results back to the user who is requesting them
 
-
 class Book:
     def to_dict(self):
         return {
             'id': self.id,
-            'title': self.brand,
-            'author': self.name,
+            'title': self.title,
+            'author': self.author,
             'year': self.year
         }
 
 # class Fiction here:
+class Fiction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40), nullable=False)
+    author = db.Column(db.String(40), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'year': self.year
+        }
 
 # class NonFiction here:
+class NonFiction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40), nullable=False)
+    author = db.Column(db.String(40), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'year': self.year
+        }
+
 
 
