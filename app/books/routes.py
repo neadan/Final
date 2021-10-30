15eec0fde74f3605742755c771f13655fc1d0275
fr4nkenstein1 <93436363+fr4nkenstein1@users.noreply.github.com>
2021-10-30 14:26:58 -0400

    Final submission

--- app/books/routes.py
+++ app/books/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify, Response
+
+from app.models import Fiction, db, NonFiction, Book
 
 book_bp = Blueprint('books', __name__, url_prefix='/api/v1/books')
 
@@ -8,14 +10,28 @@ book_bp = Blueprint('books', __name__, url_prefix='/api/v1/books')
 @book_bp.route("/fiction", methods=['GET', 'POST'])
 def books_fiction():
     ###### Return all books that are Fiction ######
-    ###### Accept the "author" query parameter, which returns all books that are written by that specific author ######
     if request.method == 'GET':
-        return "implement this GET (fiction)"
+        fiction = Fiction.query.all()
+        result = [fictions.to_dict() for fictions in fiction]
+        return jsonify(result)
+
+    ###### Accept the "author" query parameter, which returns all books that are written by that specific author ######
+    elif request.method == 'GET':
+        query_params = request.args
+        fiction = Fiction.query.filter(Fiction.year == query_params['year'])
+        result = [fictions.to_dict() for fictions in fiction]
+        return jsonify(result)
 
     ###### Create a new book in the database ######
     ###### Don't forget to return the full path of the resource in the location header ######
     if request.method == 'POST':
-        return "implement this POST (fiction)"
+        data = request.form
+        book_to_add = Fiction(title=data['title'], author=data['author'], year=data['year'])
+        db.session.add(book_to_add)
+        db.session.commit()
+        response = Response(status=200)
+        response.headers['location'] = f"/fiction/{book_to_add.id}"
+        return response
 
 
 @book_bp.route("/fiction/<int:book_id>", methods=['GET', 'DELETE'])
@@ -23,15 +39,24 @@ def book_id_fiction(book_id):
     ###### Return the book with book_id ######
     ###### If no such book exists, return the appropriate response as we have seen in class ######
     if request.method == 'GET':
-        return "implement this GET (book_id)"
+        book = Fiction.query.get(book_id)
+        return jsonify(book.to_dict())
     ###### Delete the book with no ######
     ###### If no such book exists, return the appropriate response as we have seen in class ######
     if request.method == 'DELETE':
-        return "implement this DELETE (book_id)"
+        fiction = Fiction.query.get(book_id)
+        db.session.delete(fiction)
+        db.session.commit()
+        response = Response(status=200)
+        return response
 
 
 @book_bp.route("/all", methods=['GET'])
 def books():
     ###### Return all books (whether they are fiction or non-fiction ######
     if request.method == 'GET':
-        return "implement this GET (all)"
+        fiction = Fiction.query.all()
+        nonfiction = NonFiction.query.all()
+        allbooks = fiction + nonfiction
+        result = [book.to_dict() for book in allbooks]
+        return jsonify(result)
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 # Create both a Fiction AND a NonFiction model with the following attributes:
 # id (Integer),
 # title (String),
@@ -16,13 +17,23 @@ class Book:
     def to_dict(self):
         return {
             'id': self.id,
-            'title': self.brand,
-            'author': self.name,
+            'title': self.title,         # thats sneaky way of putting a error glad i learned how to interpret errors
+            'author': self.author,
             'year': self.year
         }
 
+
 # class Fiction here:
+class Fiction(db.Model, Book):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(10), nullable=False)
+    author = db.Column(db.String(15), nullable=False)
+    year = db.Column(db.Integer)
+
 
 # class NonFiction here:
-
-
+class NonFiction(db.Model, Book):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(10), nullable=False)
+    author = db.Column(db.String(15), nullable=False)
+    year = db.Column(db.Integer)
